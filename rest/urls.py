e39de93d396138aed00550d70ff6406499d25404
Jahongir1202 <otadjonovjahongir8@gamil.com>
2024-07-20 02:59:18 +0500

    delete api

--- rest/urls.py
+++ rest/urls.py
@@ -1,13 +1,28 @@
+from sys import path_hooks
+from unittest.mock import patch
+
 from django.urls import path
+
+from api.views import AllModelsListView
 from rest.views import MoviaCinemaView, VideoCinemaView, MoviaSerialView, VideoSerialView, MoviaMultifilmView, \
-    VideoMultifilmView, MoviaAllView
+    VideoMultifilmView, MoviaAllView, MoviaJangariView, MoviaHayotiyView, MoviaDramaView, MoviaUjasView, \
+    MoviaRomanceView, MoviaFantastikView, MoviakomediaView, MoviaSearchView
 
 urlpatterns = [
-    path('', MoviaAllView.as_view(), name='movia_all'),
+    path('',MoviaAllView.as_view(),name='all'),
     path('cinema/', MoviaCinemaView.as_view(), name='cinema'),
     path('cinema/<int:pk>/', VideoCinemaView.as_view(), name='id_cinema'),
     path('serial/', MoviaSerialView.as_view(), name='serial'),
     path('serial/<int:pk>/', VideoSerialView.as_view(), name='id_serial'),
     path('multifilm/', MoviaMultifilmView.as_view(), name='multifilm'),
     path('multifilm/<int:pk>/', VideoMultifilmView.as_view(), name='id_multifilm'),
+    path('jangari/',MoviaJangariView.as_view(),name='jangari'),
+    path('komedia/', MoviakomediaView.as_view(), name='komedia'),
+    path('drama/', MoviaDramaView.as_view(), name='drama'),
+    path('ujas/', MoviaUjasView.as_view(), name='ujas'),
+    path('romance/', MoviaRomanceView.as_view(), name='romance'),
+    path('fantastik/', MoviaFantastikView.as_view(), name='fantastik'),
+    path('hayotiy/', MoviaHayotiyView.as_view(), name='hayotiy'),
+    path('search/',MoviaSearchView.as_view(),name='search')
+
 ]
